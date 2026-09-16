691d67e12e6742a9e3fc4cbab3a85ddcc87f10e6
Nikita Shekhawat <nikitashekhawat003@gmail.com>
2026-09-16 23:43:33 +0530

    Add allowed origin for production frontend

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,10 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://wemart-frontend-six.vercel.app'
+];
 
 // Middleware Configuration
 app.use(express.json()); 
